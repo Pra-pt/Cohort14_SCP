e5ea626f5d17b9a50e95fb844c37ad5aac85eb03
Ishani Mahajan <ishanimahajan@github.com>
2016-12-05 17:32:35 +0530

    adding join functionality

---
+++ client/message_handling.h
@@ -0,0 +1,30 @@
+
+#define JOIN_REQUEST 0                                                        
+#define DATA 1                                                                  
+#define UPDATE_REQUEST 2                                                        
+                               
+/*flags
+ * CONT : continue                                                              
+ * ACK:                                                                         
+ * END                                                                          
+ * */                                                                           
+                                                                                
+enum Flags{                                                                     
+    CONT,                                                                       
+    ACK,                                                                        
+    END                                                                         
+};                                                                              
+struct Mesg_header{                                                             
+     unsigned type:4;                                                           
+     unsigned group_id:10;                                                      
+     unsigned flags:8;                                                          
+     char payload[0];  //check on its memory allocation when data is received.  
+};                                                                              
+
+//States that a client can have                                                                                                                                                
+enum States{                                                                   
+    UNKNOWN,                                                                    
+    JOINING,                                                                    
+    TASK_HANDLE                                                                 
+};                                                                              
+                      
